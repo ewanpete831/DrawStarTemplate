a0156fcbd8a72be5cb2bc22963a6f502e227c96b
ewanpete831 <ewanpete831@outlook.com>
2022-02-17 09:55:05 -0500

    FInished

--- DrawingStarMethod/Form1.Designer.cs
+++ DrawingStarMethod/Form1.Designer.cs
@@ -37,13 +37,15 @@
             this.yInput = new System.Windows.Forms.TextBox();
             this.sizeInput = new System.Windows.Forms.TextBox();
             this.fillButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // drawButton
             // 
-            this.drawButton.Location = new System.Drawing.Point(283, 86);
+            this.drawButton.Location = new System.Drawing.Point(527, 119);
+            this.drawButton.Margin = new System.Windows.Forms.Padding(4);
             this.drawButton.Name = "drawButton";
-            this.drawButton.Size = new System.Drawing.Size(71, 31);
+            this.drawButton.Size = new System.Drawing.Size(85, 37);
             this.drawButton.TabIndex = 0;
             this.drawButton.Text = "Draw";
             this.drawButton.UseVisualStyleBackColor = true;
@@ -52,67 +54,91 @@
             // xLabel
             // 
             this.xLabel.AutoSize = true;
-            this.xLabel.Location = new System.Drawing.Point(261, 10);
+            this.xLabel.BackColor = System.Drawing.Color.Transparent;
+            this.xLabel.Font = new System.Drawing.Font("Comic Sans MS", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xLabel.Location = new System.Drawing.Point(498, 12);
+            this.xLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.xLabel.Name = "xLabel";
-            this.xLabel.Size = new System.Drawing.Size(15, 15);
+            this.xLabel.Size = new System.Drawing.Size(22, 23);
             this.xLabel.TabIndex = 1;
             this.xLabel.Text = "X";
             // 
             // yLabel
             // 
             this.yLabel.AutoSize = true;
-            this.yLabel.Location = new System.Drawing.Point(261, 37);
+            this.yLabel.BackColor = System.Drawing.Color.Transparent;
+            this.yLabel.Font = new System.Drawing.Font("Comic Sans MS", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yLabel.Location = new System.Drawing.Point(498, 44);
+            this.yLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.yLabel.Name = "yLabel";
-            this.yLabel.Size = new System.Drawing.Size(14, 15);
+            this.yLabel.Size = new System.Drawing.Size(21, 23);
             this.yLabel.TabIndex = 2;
             this.yLabel.Text = "Y";
             // 
             // sizeLabel
             // 
-            this.sizeLabel.AutoSize = true;
-            this.sizeLabel.Location = new System.Drawing.Point(183, 63);
+            this.sizeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.sizeLabel.Font = new System.Drawing.Font("Comic Sans MS", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sizeLabel.Location = new System.Drawing.Point(379, 75);
+            this.sizeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.sizeLabel.Name = "sizeLabel";
-            this.sizeLabel.Size = new System.Drawing.Size(109, 15);
+            this.sizeLabel.Size = new System.Drawing.Size(154, 31);
             this.sizeLabel.TabIndex = 3;
             this.sizeLabel.Text = "Size (pixel square)";
             // 
             // xInput
             // 
-            this.xInput.Location = new System.Drawing.Point(283, 7);
+            this.xInput.Location = new System.Drawing.Point(528, 12);
+            this.xInput.Margin = new System.Windows.Forms.Padding(4);
             this.xInput.Name = "xInput";
-            this.xInput.Size = new System.Drawing.Size(71, 20);
+            this.xInput.Size = new System.Drawing.Size(84, 20);
             this.xInput.TabIndex = 4;
             // 
             // yInput
             // 
-            this.yInput.Location = new System.Drawing.Point(283, 34);
+            this.yInput.Location = new System.Drawing.Point(528, 44);
+            this.yInput.Margin = new System.Windows.Forms.Padding(4);
             this.yInput.Name = "yInput";
-            this.yInput.Size = new System.Drawing.Size(71, 20);
+            this.yInput.Size = new System.Drawing.Size(84, 20);
             this.yInput.TabIndex = 5;
             // 
             // sizeInput
             // 
-            this.sizeInput.Location = new System.Drawing.Point(283, 60);
+            this.sizeInput.Location = new System.Drawing.Point(528, 79);
+            this.sizeInput.Margin = new System.Windows.Forms.Padding(4);
             this.sizeInput.Name = "sizeInput";
-            this.sizeInput.Size = new System.Drawing.Size(71, 20);
+            this.sizeInput.Size = new System.Drawing.Size(84, 20);
             this.sizeInput.TabIndex = 6;
             // 
             // fillButton
             // 
-            this.fillButton.Location = new System.Drawing.Point(283, 123);
+            this.fillButton.Location = new System.Drawing.Point(528, 164);
+            this.fillButton.Margin = new System.Windows.Forms.Padding(4);
             this.fillButton.Name = "fillButton";
-            this.fillButton.Size = new System.Drawing.Size(71, 31);
+            this.fillButton.Size = new System.Drawing.Size(85, 37);
             this.fillButton.TabIndex = 7;
             this.fillButton.Text = "Fill";
             this.fillButton.UseVisualStyleBackColor = true;
             this.fillButton.Click += new System.EventHandler(this.fillButton_Click);
             // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(527, 209);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(85, 37);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.DarkKhaki;
-            this.ClientSize = new System.Drawing.Size(366, 312);
+            this.ClientSize = new System.Drawing.Size(625, 544);
+            this.Controls.Add(this.clearButton);
             this.Controls.Add(this.fillButton);
             this.Controls.Add(this.sizeInput);
             this.Controls.Add(this.yInput);
@@ -122,6 +148,7 @@
             this.Controls.Add(this.xLabel);
             this.Controls.Add(this.drawButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DrawStar";
@@ -140,6 +167,7 @@
         private System.Windows.Forms.TextBox yInput;
         private System.Windows.Forms.TextBox sizeInput;
         private System.Windows.Forms.Button fillButton;
+        private System.Windows.Forms.Button clearButton;
     }
 }
 
--- DrawingStarMethod/Form1.cs
+++ DrawingStarMethod/Form1.cs
@@ -12,6 +12,10 @@ namespace DrawingStarMethod
 {
     public partial class Form1 : Form
     {
+        PointF[] star = new PointF[10];
+        SolidBrush blueBrush = new SolidBrush(Color.LightSteelBlue);
+        Pen blackPen = new Pen(Color.Black);
+
         public Form1()
         {
             InitializeComponent();
@@ -19,31 +23,78 @@ namespace DrawingStarMethod
 
         private void drawButton_Click(object sender, EventArgs e)
         {
-            ///The following line of code demonstrates how to get input
-            ///and convert it to a float value. Use this to help you in 
-            ///Parts 2 - 4 where you need to get inputs from the 
-            ///user and store them in float variables. This code is 
-            ///not needed for Part 1.
-
-            ///float size = Convert.ToSingle(sizeInput.Text); 
-            
-
             Graphics g = this.CreateGraphics();
-            Pen blackPen = new Pen(Color.Black);
-            
 
+            CreatePoints();
+
+            g.DrawPolygon(blackPen, star);
+
+            sizeInput.Clear();
+            xInput.Clear();
+            yInput.Clear();
         }
-
 
         private void fillButton_Click(object sender, EventArgs e)
         {
+            Graphics g = this.CreateGraphics();
 
+            CreatePoints();
+
+            g.FillPolygon(blueBrush, star);
+
+            sizeInput.Clear();
+            xInput.Clear();
+            yInput.Clear();
         }
-
-        public void DrawStar(Pen starPen, float x, float y, float pixels)
+        private void clearButton_Click(object sender, EventArgs e)
         {
-  
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.DarkKhaki);
         }
 
+        private void CreatePoints()
+        {
+            float size;
+            float starX;
+            float starY;
+
+            try
+            {
+                size = Convert.ToSingle(sizeInput.Text);
+            }
+            catch
+            {
+                size = 130;
+            }
+            try
+            {
+                starX = Convert.ToSingle(xInput.Text);
+            }
+            catch
+            {
+                starX = 0;
+            }
+            try
+            {
+                starY = Convert.ToSingle(yInput.Text);
+            }
+            catch
+            {
+                starY = 0;
+            }
+
+            float scale = size / 130;
+
+            star[0] = new PointF((65 * scale) + starX, (0 * scale) + starY);
+            star[1] = new PointF((80 * scale) + starX, (50 * scale) + starY);
+            star[2] = new PointF((130 * scale) + starX, (50 * scale) + starY);
+            star[3] = new PointF((90 * scale) + starX, (80 * scale) + starY);
+            star[4] = new PointF((105 * scale) + starX, (128 * scale) + starY);
+            star[5] = new PointF((65 * scale) + starX, (100 * scale) + starY);
+            star[6] = new PointF((25 * scale) + starX, (128 * scale) + starY);
+            star[7] = new PointF((40 * scale) + starX, (80 * scale) + starY);
+            star[8] = new PointF((0 * scale) + starX, (50 * scale) + starY);
+            star[9] = new PointF((50 * scale) + starX, (50 * scale) + starY);
+        }   
     }
 }
